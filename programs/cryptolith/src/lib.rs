01a1d4a9ff7e335fc5b622734c06ac5db17c4e4d
Standaa <stanislasdrg@gmail.com>
2021-06-04 18:27:25 +0200

    wip: Start designing programs

--- programs/cryptolith/src/lib.rs
+++ programs/cryptolith/src/lib.rs
@@ -1,12 +1,300 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Mint, MintTo, TokenAccount, Transfer};
+
+const CRYPTOLITH_SEED: &str = "WELCOMETOTHECRYPTOLITHICAGE";
 
 #[program]
-pub mod cryptolith {
+mod cryptolith {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+
+    #[state]
+    pub struct CryptolithState {
+        pub initialized: bool,
+        pub nonce: u8,
+        pub authority: Pubkey,
+        pub signer: Pubkey,
+        pub lith_token_mint: Pubkey,
+        pub lith_token_account: Pubkey,
+        pub lith_total_supply: u64,
+        pub cryptoliths: Vec<Cryptolith>,
+    }
+
+    impl CryptolithState {
+        pub fn new(_ctx: Context<New>) -> Result<Self> {
+            Ok(Self {
+                initialized: false,
+                nonce: 0,
+                authority: Pubkey::default(),
+                signer: Pubkey::default(),
+                lith_token_mint: Pubkey::default(),
+                lith_token_account: Pubkey::default(),
+                lith_total_supply: 0,
+                cryptoliths: Vec::new(),
+            })
+        }
+
+        pub fn initialize(
+            &mut self,
+            ctx: Context<Initialize>,
+            nonce: u8,
+            authority: Pubkey,
+            signer: Pubkey,
+        ) -> Result<()> {
+            msg!("Initialize Cryptolith state!");
+            self.initialized = true;
+            self.nonce = nonce;
+            self.signer = signer;
+            self.authority = authority;
+            self.lith_token_mint = *ctx.accounts.lith_token_mint.to_account_info().key; // Token Mint Account pub key
+            self.lith_token_account = *ctx.accounts.lith_token_account.to_account_info().key; // Token Mint Account pub key
+
+            // Mint total token supply to deployer account
+            let amount = u64::pow(10, 8);
+            self.lith_total_supply = amount.clone();
+
+            let seeds = &[CRYPTOLITH_SEED.as_bytes(), &[self.nonce]];
+            let signer = &[&seeds[..]];
+
+            let cpi_ctx = CpiContext::new_with_signer(
+                ctx.accounts.token_program.clone(),
+                MintTo {
+                    mint: ctx.accounts.lith_token_mint.to_account_info(),
+                    to: ctx.accounts.lith_token_account.to_account_info(),
+                    authority: ctx.accounts.lith_token_mint_authority.to_account_info(),
+                },
+                signer,
+            );
+            token::mint_to(cpi_ctx, amount)?;
+
+            Ok(())
+        }
+
+        pub fn create_cryptolith(&mut self, ctx: Context<CreateCryptolith>) -> Result<()> {
+            let cryptolith = Cryptolith {
+                id: Pubkey::default(),
+                patrons: 0,
+                latitude: 48680752,
+                longitude: 2319358,
+                height: 3,
+                funding_amount: 0,
+                realisation_amount: 20000,
+                mint_account: Pubkey::default(),
+                // mint_account: *ctx.accounts.lithn_token_mint.to_account_info().key,
+                authority: self.authority,
+            };
+
+            self.cryptoliths = vec![cryptolith.clone()];
+
+            Ok(())
+        }
+
+        // pub fn transfer_lith(&mut self, ctx: Context<LithTransfer>, amount: u64) -> Result<()> {
+        //     if amount == 0 {
+        //         return Err(ErrorCode::DepositZero.into());
+        //     }
+        //     if amount > u64::MAX {
+        //         return Err(ErrorCode::DepositTooBig.into());
+        //     }
+
+        //     let seeds = &[CRYPTOLITH_SEED.as_bytes(), &[self.nonce]];
+        //     let signer = &[&seeds[..]];
+
+        //     let cpi_ctx = CpiContext::new_with_signer(
+        //         ctx.accounts.token_program.clone(),
+        //         Transfer {
+        //             from: ctx.accounts.from_account.to_account_info(),
+        //             to: ctx.accounts.to_account.to_account_info(),
+        //             authority: ctx.accounts.lith_token_mint_authority.to_account_info(),
+        //         },
+        //         signer,
+        //     );
+        //     token::transfer(cpi_ctx, amount)?;
+        //     Ok(())
+        // }
+
+        // pub fn contribute_cryptolith(
+        //     &mut self,
+        //     ctx: Context<Contribute>,
+        //     amount: u64,
+        // ) -> Result<()> {
+        //     if amount == 0 {
+        //         return Err(ErrorCode::DepositZero.into());
+        //     }
+        //     if amount > u64::MAX {
+        //         return Err(ErrorCode::DepositTooBig.into());
+        //     }
+
+        //     let seeds = &[CRYPTOLITH_SEED.as_bytes(), &[self.nonce]];
+        //     let signer = &[&seeds[..]];
+
+        //     let cpi_ctx = CpiContext::new_with_signer(
+        //         ctx.accounts.token_program.clone(),
+        //         Transfer {
+        //             from: ctx.accounts.user_associated_token_account.to_account_info(),
+        //             to: ctx.accounts.lith_token_account.to_account_info(),
+        //             authority: ctx.accounts.lith_token_mint_authority.to_account_info(),
+        //         },
+        //         signer,
+        //     );
+        //     token::transfer(cpi_ctx, amount)?;
+
+        //     Ok(())
+        // }
+
+        // pub fn withdraw(&mut self, ctx: Context<Withdraw>, amount: u64) -> Result<()> {
+        //     if amount == 0 {
+        //         return Err(ErrorCode::WithdrawalZero.into());
+        //     }
+        //     if amount > u64::MAX {
+        //         return Err(ErrorCode::WithdrawalTooBig.into());
+        //     }
+        //     let user_balance = ctx.accounts.user_associated_token_account.amount;
+
+        //     if user_balance < amount {
+        //         return Err(ErrorCode::WithdrawalBalanceConflict.into());
+        //     }
+        //     let cpi_ctx = CpiContext::new(
+        //         ctx.accounts.token_program.clone(),
+        //         Transfer {
+        //             from: ctx.accounts.user_associated_token_account.to_account_info(),
+        //             to: ctx.accounts.lith_token_account.to_account_info(),
+        //             authority: ctx.accounts.authority.to_account_info(),
+        //         },
+        //     );
+        //     token::transfer(cpi_ctx, amount)?;
+
+        //     Ok(())
+        // }
+    }
+
+    pub fn initialize_user_account(ctx: Context<InitializeUserAccount>) -> ProgramResult {
+        let user_account = &mut ctx.accounts.user_account;
+        // user_account.shares = 0;
+        // user_account.collateral = 0;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct New {}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(mut, "lith_token_mint.decimals == 8")]
+    lith_token_mint: CpiAccount<'info, Mint>,
+    #[account(mut)]
+    lith_token_account: CpiAccount<'info, TokenAccount>,
+    lith_token_mint_authority: AccountInfo<'info>,
+    #[account("token_program.key == &token::ID")]
+    token_program: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct CreateCryptolith<'info> {
+    // #[account(signer)]
+    // authority: AccountInfo<'info>,
+    #[account(mut, "lithn_token_mint.decimals == 8")]
+    lithn_token_mint: CpiAccount<'info, Mint>,
+}
+
+#[derive(Accounts)]
+pub struct InitializeUserAccount<'info> {
+    #[account(associated = authority, space = 256 )]
+    user_account: ProgramAccount<'info, UserCryptolithAccount>,
+    #[account(mut, signer)]
+    authority: AccountInfo<'info>,
+    rent: Sysvar<'info, Rent>,
+    system_program: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Contribute<'info> {
+    #[account(mut)]
+    pub lith_token_account: CpiAccount<'info, TokenAccount>,
+    //TODO: Check ACL
+    #[account(mut)]
+    pub lith_token_mint_authority: AccountInfo<'info>,
+    #[account(mut)]
+    pub user_associated_token_account: CpiAccount<'info, TokenAccount>,
+    #[account("token_program.key == &token::ID")]
+    pub token_program: AccountInfo<'info>,
+}
+
+// #[derive(Accounts)]
+// pub struct LithTransfer<'info> {
+//     #[account(mut)]
+//     pub lith_token_mint_authority: AccountInfo<'info>,
+//     #[account(mut)]
+//     pub from_account: CpiAccount<'info, TokenAccount>,
+//     #[account(mut)]
+//     pub to_account: CpiAccount<'info, TokenAccount>,
+//     #[account("token_program.key == &token::ID")]
+//     pub token_program: AccountInfo<'info>,
+// }
+
+// #[derive(Accounts)]
+// pub struct Withdraw<'info> {
+//     #[account(mut)]
+//     pub lith_token_account: CpiAccount<'info, TokenAccount>,
+//     //TODO: Check ACL
+//     #[account(mut)]
+//     pub lith_token_mint_authority: AccountInfo<'info>,
+//     #[account(mut)]
+//     pub user_associated_token_account: CpiAccount<'info, TokenAccount>,
+//     #[account("token_program.key == &token::ID")]
+//     pub token_program: AccountInfo<'info>,
+//     #[account(signer)]
+//     pub authority: AccountInfo<'info>,
+// }
+
+#[associated]
+pub struct UserCryptolithAccount {
+    pub cryptoliths: Vec<Contribution>,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone)]
+pub struct Cryptolith {
+    pub id: Pubkey,
+    pub patrons: u32,
+    pub latitude: i32,
+    pub longitude: i32,
+    pub height: u8,
+    pub funding_amount: u64,
+    pub realisation_amount: u64,
+    pub mint_account: Pubkey,
+    pub authority: Pubkey,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone)]
+pub struct Contribution {
+    pub cryptolith_id: Pubkey,
+    pub lith_invested: u64,
+}
+
+#[error]
+pub enum ErrorCode {
+    #[msg("Access unauthorized")]
+    AccessControl,
+    #[msg("Cannot deposit zero")]
+    DepositZero,
+    #[msg("Deposit Amount too small")]
+    DepositTooSmall,
+    #[msg("Deposit Amount too large")]
+    DepositTooBig,
+    #[msg("Cannot withdraw zero")]
+    WithdrawalZero,
+    #[msg("Withdrawal Amount too small")]
+    WithdrawalTooSmall,
+    #[msg("Withdrawal Amount too large")]
+    WithdrawalTooBig,
+    #[msg("Cannot withdraw more than holdings")]
+    WithdrawalBalanceConflict,
+    #[msg("Cannot lock zero")]
+    LockZero,
+    #[msg("Lock Amount too large")]
+    LockTooBig,
+    #[msg("Cannot unlock zero")]
+    UnlockZero,
+    #[msg("Unlock Amount too large")]
+    UnlockTooBig,
+}
--- tests/cryptolith.spec.ts
+++ tests/cryptolith.spec.ts
@@ -1,14 +1,383 @@
-import * as anchor from '@project-serum/anchor';
+import assert from "assert";
+import { web3, Provider, workspace, setProvider, BN, Wallet } from "@project-serum/anchor";
+import { Token, TOKEN_PROGRAM_ID } from "@solana/spl-token";
 
-describe('cryptolith', () => {
-
+describe("cryptolith", () => {
+  const provider = Provider.local();
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  setProvider(provider);
+  // Program for the tests.
+  const cryptolithProgram = workspace.Cryptolith;
+  // @ts-expect-error
+  const payer = provider.wallet.payer as web3.Account;
+  const CRYPTOLITH_SEED = Buffer.from("WELCOMETOTHECRYPTOLITHICAGE");
+  const CRYPTOLITHN_SEED = Buffer.from("PAMPIT");
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const program = anchor.workspace.Cryptolith;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
+  let cryptolithAuthority: web3.PublicKey;
+  let nonce: number;
+  let lithTokenMint: Token;
+  let lithTokenAccount: web3.PublicKey;
+  let userAssociatedAddress: web3.PublicKey;
+
+  before(async () => {
+    const [_cryptolithAuthority, _nonce] = await web3.PublicKey.findProgramAddress(
+      [CRYPTOLITH_SEED],
+      cryptolithProgram.programId,
+    );
+
+    cryptolithAuthority = _cryptolithAuthority;
+    nonce = _nonce;
+
+    lithTokenMint = await Token.createMint(
+      provider.connection,
+      payer,
+      cryptolithAuthority,
+      null,
+      8,
+      TOKEN_PROGRAM_ID,
+    );
+
+    console.log("lithTokenMint", (await lithTokenMint.getMintInfo()).mintAuthority.toBase58());
+
+    lithTokenAccount = await lithTokenMint.createAccount(cryptolithAuthority);
+    userAssociatedAddress = await lithTokenMint.createAssociatedTokenAccount(provider.wallet.publicKey);
+
+    console.log("lithTokenAccount", lithTokenAccount.toBase58());
+    console.log("userAssociatedAddress", userAssociatedAddress.toBase58());
   });
+
+  it("Initializes Cryptolith State correctly", async () => {
+    await cryptolithProgram.state.rpc.new({
+      accounts: {},
+    });
+
+    await cryptolithProgram.state.rpc.initialize(
+      nonce,
+      provider.wallet.publicKey,
+      provider.wallet.publicKey,
+      {
+        accounts: {
+          lithTokenMint: lithTokenMint.publicKey,
+          lithTokenAccount: lithTokenAccount,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          lithTokenMintAuthority: cryptolithAuthority,
+        },
+      },
+    );
+
+    const cryptolithStateAfterInit = await cryptolithProgram.state.fetch();
+    assert.ok(cryptolithStateAfterInit.nonce === nonce);
+    assert.ok(cryptolithStateAfterInit.signer.equals(provider.wallet.publicKey));
+    assert.ok(cryptolithStateAfterInit.authority.equals(provider.wallet.publicKey));
+    assert.ok(cryptolithStateAfterInit.lithTokenMint.equals(lithTokenMint.publicKey));
+    assert.ok(cryptolithStateAfterInit.lithTokenAccount.equals(lithTokenAccount));
+    assert.ok(cryptolithStateAfterInit.lithTotalSupply.toNumber() === 10e7);
+
+    const lithTokenAccountAfterMint = await lithTokenMint.getAccountInfo(lithTokenAccount);
+    // @ts-ignore
+    assert.ok(lithTokenAccountAfterMint.amount.toNumber() === 10e7);
+  });
+
+  it("Creates a new Cryptolith", async () => {
+    const [_cryptolithAuthority, _nonce] = await web3.PublicKey.findProgramAddress(
+      [CRYPTOLITHN_SEED],
+      cryptolithProgram.programId,
+    );
+
+    cryptolithAuthority = _cryptolithAuthority;
+    nonce = _nonce;
+
+    const lithnTokenMint = await Token.createMint(
+      provider.connection,
+      payer,
+      cryptolithAuthority,
+      null,
+      8,
+      TOKEN_PROGRAM_ID,
+    );
+
+    console.log("lithnTokenMint", (await lithnTokenMint.getMintInfo()).mintAuthority.toBase58());
+    const lithnTokenAccount = await lithnTokenMint.createAccount(cryptolithAuthority);
+    const userLithnAssociatedAddress = await lithnTokenMint.createAssociatedTokenAccount(
+      provider.wallet.publicKey,
+    );
+
+    console.log("lithnTokenAccount", lithnTokenAccount.toBase58());
+    console.log("userLithnAssociatedAddress", userLithnAssociatedAddress.toBase58());
+
+    console.log("lithnTokenMint.publicKey", lithnTokenMint.publicKey.toBase58());
+
+    await cryptolithProgram.state.rpc.createCryptolith({
+      accounts: {
+        lithnTokenMint: lithnTokenMint.publicKey,
+        // authority: provider.wallet.publicKey,
+      },
+    });
+
+    assert.ok(true);
+  });
+
+  // it("Deposits a first time from wallet to user account", async () => {
+  //   const cryptolithStateAfterInit = await cryptolithProgram.state();
+  //   const mintAddress: web3.PublicKey = cryptolithStateAfterInit.lithTokenMint;
+
+  //   // Refetch the token instead of getting lithTokenMint directly
+  //   const lithTokenMintAfterInit = new Token(provider.connection, mintAddress, TOKEN_PROGRAM_ID, payer);
+
+  //   await cryptolithProgram.state.rpc.deposit(new BN(50000), {
+  //     accounts: {
+  //       lithTokenMintAuthority: cryptolithAuthority,
+  //       lithTokenAccount: lithTokenAccount,
+  //       userAssociatedTokenAccount: userAssociatedAddress,
+  //       tokenProgram: TOKEN_PROGRAM_ID,
+  //     },
+  //   });
+
+  //   const lithTokenAccountAfterDeposit = await lithTokenMintAfterInit.getAccountInfo(lithTokenAccount);
+
+  //   const userTokenAccountAfterDeposit = await lithTokenMintAfterInit.getAccountInfo(userAssociatedAddress);
+
+  //   const cryptolithStateAfterDepositAfterInit = await cryptolithProgram.state();
+
+  //   assert.ok(lithTokenAccountAfterDeposit.amount.toNumber() === 99950000);
+  //   assert.ok(userTokenAccountAfterDeposit.amount.toNumber() === 50000);
+  //   assert.ok(cryptolithStateAfterDepositAfterInit.collateralBalance.toNumber() === 50000);
+  // });
+
+  // it("Deposits a second time from wallet to user account", async () => {
+  //   const cryptolithStateAfterInit = await cryptolithProgram.state();
+  //   const mintAddress: web3.PublicKey = cryptolithStateAfterInit.lithTokenMint;
+
+  //   // Refetch the token instead of getting lithTokenMint directly
+  //   const lithTokenMintAfterInit = new Token(provider.connection, mintAddress, TOKEN_PROGRAM_ID, payer);
+
+  //   await cryptolithProgram.state.rpc.deposit(new BN(50000), {
+  //     accounts: {
+  //       lithTokenMintAuthority: cryptolithAuthority,
+  //       lithTokenAccount: lithTokenAccount,
+  //       userAssociatedTokenAccount: userAssociatedAddress,
+  //       tokenProgram: TOKEN_PROGRAM_ID,
+  //     },
+  //   });
+
+  //   const lithTokenAccountAfterDeposit = await lithTokenMintAfterInit.getAccountInfo(lithTokenAccount);
+
+  //   const userTokenAccountAfterDeposit = await lithTokenMintAfterInit.getAccountInfo(userAssociatedAddress);
+
+  //   const cryptolithStateAfterDepositAfterInit = await cryptolithProgram.state();
+
+  //   assert.ok(lithTokenAccountAfterDeposit.amount.toNumber() === 99900000);
+  //   assert.ok(userTokenAccountAfterDeposit.amount.toNumber() === 100000);
+  //   assert.ok(cryptolithStateAfterDepositAfterInit.collateralBalance.toNumber() === 100000);
+  // });
+
+  // it("Should return an error if deposit is zero", async () => {
+  //   let err: string;
+  //   try {
+  //     await cryptolithProgram.state.rpc.deposit(new BN(0), {
+  //       accounts: {
+  //         lithTokenMintAuthority: cryptolithAuthority,
+  //         lithTokenAccount: lithTokenAccount,
+  //         userAssociatedTokenAccount: userAssociatedAddress,
+  //         tokenProgram: TOKEN_PROGRAM_ID,
+  //       },
+  //     });
+  //   } catch (e) {
+  //     err = e.toString();
+  //   }
+  //   assert.ok(err === "Cannot deposit zero");
+  // });
+
+  // it("Withdraws a first time from user account to wallet", async () => {
+  //   const cryptolithStateAfterInit = await cryptolithProgram.state();
+  //   const mintAddress: web3.PublicKey = cryptolithStateAfterInit.lithTokenMint;
+
+  //   const lithTokenMintAfterInit = new Token(provider.connection, mintAddress, TOKEN_PROGRAM_ID, payer);
+
+  //   await cryptolithProgram.state.rpc.withdraw(new BN(10), {
+  //     accounts: {
+  //       lithTokenMintAuthority: cryptolithAuthority,
+  //       lithTokenAccount: lithTokenAccount,
+  //       userAssociatedTokenAccount: userAssociatedAddress,
+  //       tokenProgram: TOKEN_PROGRAM_ID,
+  //       authority: provider.wallet.publicKey,
+  //     },
+  //   });
+
+  //   const lithTokenAccountAfterWithdrawal = await lithTokenMintAfterInit.getAccountInfo(lithTokenAccount);
+  //   const userTokenAccountAfterWithdrawal = await lithTokenMintAfterInit.getAccountInfo(
+  //     userAssociatedAddress,
+  //   );
+
+  //   const cryptolithStateAfterWithdrawalAfterInit = await cryptolithProgram.state();
+
+  //   assert.ok(lithTokenAccountAfterWithdrawal.amount.toNumber() === 99900010);
+  //   assert.ok(userTokenAccountAfterWithdrawal.amount.toNumber() === 99990);
+  //   assert.ok(cryptolithStateAfterWithdrawalAfterInit.collateralBalance.toNumber() === 99990);
+  // });
+
+  // it("Withdraws a second time from user account to wallet", async () => {
+  //   const cryptolithStateAfterInit = await cryptolithProgram.state();
+  //   const mintAddress: web3.PublicKey = cryptolithStateAfterInit.lithTokenMint;
+
+  //   const lithTokenMintAfterInit = new Token(provider.connection, mintAddress, TOKEN_PROGRAM_ID, payer);
+
+  //   await cryptolithProgram.state.rpc.withdraw(new BN(10), {
+  //     accounts: {
+  //       lithTokenMintAuthority: cryptolithAuthority,
+  //       lithTokenAccount: lithTokenAccount,
+  //       userAssociatedTokenAccount: userAssociatedAddress,
+  //       tokenProgram: TOKEN_PROGRAM_ID,
+  //       authority: provider.wallet.publicKey,
+  //     },
+  //   });
+
+  //   const lithTokenAccountAfterWithdrawal = await lithTokenMintAfterInit.getAccountInfo(lithTokenAccount);
+  //   const userTokenAccountAfterWithdrawal = await lithTokenMintAfterInit.getAccountInfo(
+  //     userAssociatedAddress,
+  //   );
+
+  //   const cryptolithStateAfterWithdrawalAfterInit = await cryptolithProgram.state();
+
+  //   assert.ok(lithTokenAccountAfterWithdrawal.amount.toNumber() === 99900020);
+  //   assert.ok(userTokenAccountAfterWithdrawal.amount.toNumber() === 99980);
+  //   assert.ok(cryptolithStateAfterWithdrawalAfterInit.collateralBalance.toNumber() === 99980);
+  // });
+
+  // it("Returns an error if withdrawal bigger than deposited collateral", async () => {
+  //   let err: string;
+  //   try {
+  //     await cryptolithProgram.state.rpc.withdraw(new BN(500000), {
+  //       accounts: {
+  //         lithTokenMintAuthority: cryptolithAuthority,
+  //         lithTokenAccount: lithTokenAccount,
+  //         userAssociatedTokenAccount: userAssociatedAddress,
+  //         tokenProgram: TOKEN_PROGRAM_ID,
+  //         authority: provider.wallet.publicKey,
+  //       },
+  //     });
+  //   } catch (e) {
+  //     err = e.toString();
+  //   }
+  //   // Make sure test doesn't pass if withdrawal smaller than collateral
+  //   assert.ok(err === "Cannot withdraw more than holdings");
+  // });
+
+  // it("Returns an error if withdrawal amount is zero", async () => {
+  //   let err: string;
+  //   try {
+  //     await cryptolithProgram.state.rpc.withdraw(new BN(0), {
+  //       accounts: {
+  //         lithTokenMintAuthority: cryptolithAuthority,
+  //         lithTokenAccount: lithTokenAccount,
+  //         userAssociatedTokenAccount: userAssociatedAddress,
+  //         tokenProgram: TOKEN_PROGRAM_ID,
+  //         authority: provider.wallet.publicKey,
+  //       },
+  //     });
+  //   } catch (e) {
+  //     err = e.toString();
+  //   }
+  //   assert.ok(err === "Cannot withdraw zero");
+  // });
+
+  // it("Locks liquidity a first time", async () => {
+  //   await cryptolithProgram.state.rpc.lock(new BN(10000), {
+  //     accounts: {
+  //       authority: provider.wallet.publicKey,
+  //     },
+  //   });
+
+  //   const cryptolithStateAfterLockAfterInit = await cryptolithProgram.state();
+  //   assert.ok(cryptolithStateAfterLockAfterInit.lockedLiquidity.toNumber() === 10000);
+  // });
+
+  // it("Locks liquidity a second time", async () => {
+  //   await cryptolithProgram.state.rpc.lock(new BN(10000), {
+  //     accounts: {
+  //       authority: provider.wallet.publicKey,
+  //     },
+  //   });
+
+  //   const cryptolithStateAfterLockAfterInit = await cryptolithProgram.state();
+  //   assert.ok(cryptolithStateAfterLockAfterInit.lockedLiquidity.toNumber() === 20000);
+  // });
+
+  // it("Returns an error if lock amount is greater than 80% collateral", async () => {
+  //   let err: string;
+  //   try {
+  //     await cryptolithProgram.state.rpc.lock(new BN(100000), {
+  //       accounts: {
+  //         authority: provider.wallet.publicKey,
+  //       },
+  //     });
+  //   } catch (e) {
+  //     err = e.toString();
+  //   }
+  //   assert.ok(err === "Lock Amount too large");
+  // });
+
+  // it("Returns an error if lock amount is zero", async () => {
+  //   let err: string;
+  //   try {
+  //     await cryptolithProgram.state.rpc.lock(new BN(0), {
+  //       accounts: {
+  //         authority: provider.wallet.publicKey,
+  //       },
+  //     });
+  //   } catch (e) {
+  //     err = e.toString();
+  //   }
+  //   assert.ok(err === "Cannot lock zero");
+  // });
+
+  // it("Unlocks liquidity a first time", async () => {
+  //   await cryptolithProgram.state.rpc.unlock(new BN(10000), {
+  //     accounts: {
+  //       authority: provider.wallet.publicKey,
+  //     },
+  //   });
+
+  //   const cryptolithStateAfterLockAfterInit = await cryptolithProgram.state();
+  //   assert.ok(cryptolithStateAfterLockAfterInit.lockedLiquidity.toNumber() === 10000);
+  // });
+
+  // it("Unlocks liquidity a second time", async () => {
+  //   await cryptolithProgram.state.rpc.unlock(new BN(10000), {
+  //     accounts: {
+  //       authority: provider.wallet.publicKey,
+  //     },
+  //   });
+
+  //   const cryptolithStateAfterLockAfterInit = await cryptolithProgram.state();
+  //   assert.ok(cryptolithStateAfterLockAfterInit.lockedLiquidity.toNumber() === 0);
+  // });
+
+  // it("Returns an error if unlock amount is greater than locked liquidity", async () => {
+  //   let err: string;
+  //   try {
+  //     await cryptolithProgram.state.rpc.unlock(new BN(500000), {
+  //       accounts: {
+  //         authority: provider.wallet.publicKey,
+  //       },
+  //     });
+  //   } catch (e) {
+  //     err = e.toString();
+  //   }
+  //   assert.ok(err === "Unlock Amount too large");
+  // });
+
+  // it("Returns an error if unlock amount is zero", async () => {
+  //   let err: string;
+  //   try {
+  //     await cryptolithProgram.state.rpc.unlock(new BN(0), {
+  //       accounts: {
+  //         authority: provider.wallet.publicKey,
+  //       },
+  //     });
+  //   } catch (e) {
+  //     err = e.toString();
+  //   }
+  //   assert.ok(err === "Cannot unlock zero");
+  // });
 });
