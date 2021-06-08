74419affc2e6483f3b045c888fcddda24d706c81
Standaa <stanislasdrg@gmail.com>
2021-06-08 17:34:40 +0200

    chore: Add config file

---
+++ app/config/index.ts
@@ -0,0 +1,8 @@
+export const config = {
+  localNet: "http://localhost:8899",
+  devNet: "https://devnet.solana.com",
+  testNet: "https://testnet.solana.com",
+  mainNet: "https://api.mainnet-beta.solana.com",
+  solletWalletUrl: "https://www.sollet.io",
+  programId: "7AuP51ndgTiAPnSiwWFhh32vvGEaFZJA1gaEh8vHLcQZ",
+};
